8dd97ebd54b4d62596a74c504dad4813de46a1f1
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2019-11-18 16:38:56 +0100

    fixed vulnerabilities errors

--- build/NumberInput/NumberInput.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-var NumberInput = function () {
-    return React.createElement("input", { type: "number" });
-};
-export default NumberInput;
-//# sourceMappingURL=NumberInput.js.map
--- build/NumberInput/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-interface Props {
-}
-declare const NumberInput: React.FunctionComponent<Props>;
-export default NumberInput;
